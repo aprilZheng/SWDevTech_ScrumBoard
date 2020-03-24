b287a238172b73e5cd03f5c9da2da3959f5b3a6c
GaborFarkas <raandal73@gmail.com>
2020-03-24 12:40:33 +0100

    Implement cards

---
+++ ScrumBoard/Card.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ScrumBoard
+{
+    /// <summary>
+    /// Interaction logic for Card.xaml
+    /// </summary>
+    public partial class Card : UserControl
+    {
+        private string title_;
+        private string description_;
+
+        public string Title {
+            get
+            {
+                return title_;
+            }
+            set
+            {
+                title_ = value;
+                GuiTitle.Text = title_;
+            }
+        }
+        public string Description {
+            get
+            {
+                return description_;
+            }
+            set
+            {
+                description_ = value;
+                GuiDescription.Text = description_;
+            }
+        }
+
+        public Card()
+        {
+            InitializeComponent();
+
+            Grid grid = (Grid)Content;
+            Border card = (Border)grid.Children[0];
+            card.Background = Brushes.White;
+        }
+    }
+}
--- ScrumBoard/MainWindow.xaml.cs
+++ ScrumBoard/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ScrumBoard
 {
@@ -20,14 +9,58 @@ namespace ScrumBoard
     /// </summary>
     public partial class MainWindow : Window
     {
+        // Properties
+        public List<Card> TodoList { get; }
+        public List<Card> DoingList { get; }
+        public List<Card> DoneList { get; }
+
+        private Dictionary<string, List<Card>> labelMapping =
+            new Dictionary<string, List<Card>>();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            TodoList = new List<Card>();
+            labelMapping.Add("To Do", TodoList);
+            DoingList = new List<Card>();
+            labelMapping.Add("Doing", DoingList);
+            DoneList = new List<Card>();
+            labelMapping.Add("Done", DoneList);
         }
 
         private void btnAdd_Click(object sender, RoutedEventArgs e)
         {
+            string label = GetColumnLabel(sender);
+            List<Card> column = labelMapping[label];
 
+            Card newCard = new Card
+            {
+                Height = 150
+            };
+            newCard.Title = "Title of new card";
+            newCard.Description = "Description of new card";
+
+            column.Add(newCard);
+            GetContainer(sender).Children.Add(newCard);
+        }
+
+        private string GetColumnLabel(object sender)
+        {
+            Button senderBtn = (Button)sender;
+            Grid grid = (Grid)senderBtn.Parent;
+            TextBlock label = (TextBlock)grid.Children[1];
+
+            return label.Text;
+        }
+
+        private StackPanel GetContainer(object sender)
+        {
+            Button senderBtn = (Button)sender;
+            Grid grid = (Grid)senderBtn.Parent;
+            ScrollViewer container = (ScrollViewer)grid.Children[4];
+
+            return (StackPanel)container.Content;
         }
     }
 }
